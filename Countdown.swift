0dbe8322eaeda079439e5b61216349883af109c6
Michael Madden <mikemadden42@icloud.com>
2020-06-22 21:48:18 -0500

    Update Countdown for Swift 5.2

--- Countdown.swift
+++ Countdown.swift
@@ -1,24 +1,12 @@
-// xcrun -sdk macosx swiftc -o Countdown Countdown.swift
-// xcrun -sdk macosx swiftc Countdown.swift
-
 import Foundation
 
-// http://stackoverflow.com/questions/24723431/swift-days-between-two-nsdates
-// http://stackoverflow.com/questions/24070450/how-to-get-the-current-timeand-hour-as-datetime-swift
+// https://stackoverflow.com/questions/40075850/swift-3-find-number-of-calendar-days-between-two-dates
 
-let date = NSDate()
-let formatter = NSDateFormatter()
-formatter.dateStyle = .LongStyle
-formatter.dateFormat = "MM-dd-yyyy"
+let startDate = "2019-04-08"
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd"
+let formatedStartDate = dateFormatter.date(from: startDate)
+let currentDate = Date()
+let differenceOfDate = Calendar.current.dateComponents([.day], from: formatedStartDate!, to: currentDate)
 
-let today = formatter.stringFromDate(date)
-let end = "12-25-2016"
-
-let startDate: NSDate = formatter.dateFromString(today)!
-let endDate: NSDate = formatter.dateFromString(end)!
-
-let calendar = NSCalendar.currentCalendar()
-
-let components = calendar.components([.Day], fromDate: startDate, toDate: endDate, options: [])
-
-print("\(components.day) days until Christmas!")
+print(differenceOfDate)
