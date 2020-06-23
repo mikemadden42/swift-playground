6c39b8e4912d717423f27076bf2f35178d96c2a9
Michael Madden <mikemadden42@icloud.com>
2020-06-22 21:51:13 -0500

    Update build.sh for Countdown, Lookup

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for PROG in Chapter01 hello sysinfo; do
+for PROG in Chapter01 Countdown Lookup hello sysinfo; do
 	swiftc -O -gnone -warnings-as-errors -o ${PROG} ${PROG}.swift
 	strip ${PROG}
 	echo
