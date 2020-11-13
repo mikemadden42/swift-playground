8a972ac24cf0f00ef26c03afc7e3988bb9d8ac5e
Michael Madden <mikemadden42@icloud.com>
2020-11-13 11:19:24 -0600

    Build universal binaries.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
 for PROG in Chapter01 Countdown Lookup hello launch sysinfo; do
-	swiftc -O -gnone -warnings-as-errors -o ${PROG} ${PROG}.swift && strip ${PROG}
+	swiftc -O -gnone -warnings-as-errors -target x86_64-apple-macos10.12 -o ${PROG}-x86_64 ${PROG}.swift && strip ${PROG}-x86_64
+	swiftc -O -gnone -warnings-as-errors -target arm64-apple-macos11 -o ${PROG}-arm64 ${PROG}.swift && strip ${PROG}-arm64
+    lipo -create -output ${PROG} ${PROG}-x86_64 ${PROG}-arm64
 	echo
 done
